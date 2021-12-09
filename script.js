f96a02d9c3c573d266a50578ca61f9a8f72124cd
Lefa Mokgatle <mokgatlelefa@gmail.com>
2021-12-09 23:31:41 +0200

    changes made on the projects page

--- script.js
+++ script.js
@@ -5,10 +5,15 @@ $(document).ready(function(){
         }else {
             $('.navbar').removeClass('sticky');
         }
-    })
+    });
     // toggle menu/navbar script
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass('active')
         $('.menu-btn i').toggleClass('active')
-    })
+    });
+
+    // owl carousel script
+    $('carousel').owlCarousel({
+        
+    });
 })
